96b6dfdb21ce56f1bbf86140675be68b339601e4
blukai <init1@protonmail.com>
2016-08-07 17:19:55 +0300

    get match ids from dotabuff

--- matches.js
+++ matches.js
@@ -1,68 +1,80 @@
+let firebase = require('firebase'),
+    axios = require('axios'),
+    cheerio = require('cheerio');
+
 require('dotenv').config({silent: true});
 
-var firebase = require('firebase'),
-    axios = require('axios');
-
 firebase.initializeApp({
-  serviceAccount: './firebase.json',
+  serviceAccount: './firebase_service_account.json',
   databaseURL: process.env.FIREBASE_URL
 });
 
 var ref = firebase.database().ref('the-int'),
     matchesRef = ref.child('matches');
 
-matchesRef.once('value', function(snap) {
+let leagueId = 4664; // The International 2016
+
+matchesRef.once('value', (snap) => {
   if(snap.exists()) {
-    // matchesRef.orderByKey().limitToFirst(1).once('value', function(snap) {
-    //   console.log(Object.keys(snap.val())[0]);
-      
-    //   matchesRef.once('value', function(snap) {
-    //     console.log(snap.numChildren());
-    //   });
-    // });
-    
-    matchesRef.orderByKey().limitToFirst(1).once('value')
-      .then(function(snap) {
-        let start = Object.keys(snap.val())[0];
-        console.log('start_at_match_id: ' + start)
-        getMatches(start) && getMatches(null);
-      });
+
+    matchesRef.orderByKey().once('value', (snap) => {
+      let hasMatches = Object.keys(snap.val());
+
+      axios.get('/esports/leagues/' + leagueId + '/scores', {
+          baseURL: 'http://www.dotabuff.com/'
+      }).then((res) => {
+        let $ = cheerio.load(res.data);
+        let matches = [];
+        $('a[class=match-score]').each((i, elem) => {
+          matches[i] = $(elem).attr('href').replace(/\D/g,'');
+        });
+
+        let diff = matches.filter(x => hasMatches.indexOf(x) < 0 );
+        if(diff.length > 0) {
+          for(let i = 0; i < diff.length; i++) {
+            matchesRef.once('value', (snap) => {
+              if(!snap.child(diff[i]).exists()) {
+                data = {};
+                data[diff[i]] = {
+                  league_id: leagueId
+                };
+                matchesRef.update(data);
+
+                console.log('done: ' + diff[i]);
+              }
+            })
+          }
+        } else {
+          process.exit(console.log('up to date'));          
+        }
+      })
+    });
+
   } else {
-    getMatches(null);
+
+    axios.get('/esports/leagues/' + leagueId + '/scores', {
+      baseURL: 'http://www.dotabuff.com/'
+    }).then((res) => {
+      let $ = cheerio.load(res.data);
+      let matches = [];
+      $('a[class=match-score]').each((i, elem) => {
+        matches[i] = $(elem).attr('href').replace(/\D/g,'');
+      });
+
+      for(let i = 0; i < matches.length; i++) {
+        //console.log(matches[i]);
+        matchesRef.once('value', (snap) => {
+          if(!snap.child(matches[i]).exists()) {
+            data = {};
+            data[matches[i]] = {
+              league_id: leagueId
+            };
+            matchesRef.update(data);
+            console.log('done: ' + matches[i]);
+          }
+        })
+      }
+    })
+
   }
 });
-
-let getMatches = function(start) {
-  axios.get('/IDOTA2Match_570/GetMatchHistory/v1/', {
-      baseURL: process.env.STEAM_API_URL,
-      params: {
-        key: process.env.STEAM_API_KEY,
-        league_id: 4664,
-        // matches_requested: 3,
-        game_mode: 2,
-        min_players: 10,
-        start_at_match_id: start
-      }
-    })
-    .then(function (response) {
-      for(let i = 0; i < response.data.result.matches.length; i++) {
-        let match = response.data.result.matches[i];
-        // GMT: Tue, 02 Aug 2016 00:00:00 GMT - 1470096000
-        
-        matchesRef.once('value', function(snap) {
-          if(!snap.child(match.match_id).exists()) {
-            let data = {};
-              data[match.match_id] = {
-                series_id: match.series_id,
-                series_type: match.series_type
-              };
-              matchesRef.update(data);
-          }
-        });
-      }
-    })
-    .catch(function (error) {
-      console.log(error);
-      process.exit();
-    });
-}
