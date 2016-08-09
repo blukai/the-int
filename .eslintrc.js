292440021c1360faba4bce9a598e83a5f9b1b9d7
blukai <init1@protonmail.com>
2016-08-09 18:55:55 +0300

    eslint .js -> .json

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    "extends": "google",
-    "rules": {
-        "one-var": ["error", "always"],
-        indent: [2, 2, {"SwitchCase": 1, "VariableDeclarator": 2}],
-        camelcase: ["error", {properties: "never"}]
-    }
-};
